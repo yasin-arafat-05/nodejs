ab4054ad556ae6d152b0f092f3b39ac16fc47068
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2025-01-01 14:38:33 +0600

    data_types

---
+++ 01_basic_of_javascript/03_types_of_data.js
@@ -0,0 +1,27 @@
+
+let a = "yasin"
+let b = 12;
+let c = 12.5;
+let d = 1234123334343434234n; 
+
+var e = true;
+var f = false;
+
+// <- null value -> 
+const g = null;
+
+// <- undefined -> 
+let  h;
+var i;
+
+// <- - -  print - - -> 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+
