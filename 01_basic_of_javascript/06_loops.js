715139deab0ec1c6d9258c64e6898f2b1e3fa8c3
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2025-01-01 16:59:17 +0600

    loops_js

---
+++ 01_basic_of_javascript/06_loops.js
@@ -0,0 +1,23 @@
+
+console.log("for loop -> : ");
+for(var i=0;i<6;i++){
+    console.log(i);
+}
+
+console.log("");
+console.log("while loop -> : ");
+
+{
+    let  k = 0;
+    while(k<=5){
+        console.log(k);
+        k++;
+    }
+}
+
+console.log();
+console.log("for each loop: ");
+
+let colors  = ["red","blue",12,14,234];
+colors.forEach((fruits)=>console.log(fruits));
+
